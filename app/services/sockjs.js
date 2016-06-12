12ffe03ba004f623fab6c71a8bb31968262f33df
rroldan <roldan.ricardo@gmail.com>
2016-06-12 08:55:51 +0200

    Create a new service for SockJS

--- app/services/sockjs.js
+++ app/services/sockjs.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
+const {run} = Ember;
 
-export default Ember.Service.extend({
+export default Ember.Service.extend(Ember.Evented,{
+  socket: null,
+  init() {
+    this._super();
+    let socket = new SockJs('http:localhost:7000');
+    socket.addEventListener('message',run.bind(this,(event)=>{
+      this.trigger('messageReceived',event.data);
+      cosole.log(event.data);
+    }));
+    this.set('socket',socket);
+  },
+  sendInfo(message) {
+    this.get('socket').send(message);
+  }
 });
